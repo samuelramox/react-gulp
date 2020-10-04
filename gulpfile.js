b616d94abc44d95d7f99be40fe986cce0f238691
Samuel Ramos <samuelramox@outlook.com>
2020-10-04 08:17:21 -0300

    feat: add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
-let gulp = require('gulp');
-let reactEasy = require('gulp-react-easy');
+const gulp = require('gulp');
+const reactEasy = require('gulp-react-easy');
+const browserSync = require('browser-sync').create();
 
-gulp.task('default', ['js', 'watch']);
+gulp.task('default', ['js', 'browser-sync', 'watch']);
+
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+});
 
 gulp.task('watch', () => {
   gulp.watch('./src/js/**/*', ['js']);
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
 
 gulp.task('js', () =>
   reactEasy({ file: './src/js/app.jsx', debug: true })
     .to('app.js')
     .pipe(gulp.dest('./dist/js/'))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    )
 );
